e7c0b2e6d1923bc91b4b83672bc112f064e4efe1
Segun <miksegun1996@gmail.com>
2023-08-18 18:16:24 +0100

    injected the controller(dependency)

--- lib/home-page/home_page.dart
+++ lib/home-page/home_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_state_management/controllers/tap_controller.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TapController controller = Get.put(TapController());
     return Scaffold(
       body: SafeArea(
           child: Container(
@@ -13,6 +16,26 @@ class HomePage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                GestureDetector(
+                  onTap: (){
+                    controller.increase();
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(10),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.green,
+                    ),
+                    child: const Center(
+                      child: Text("Tap Me", style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      ),
+                    ),
+                  ),
+                ),
                 GestureDetector(
                   onTap: (){},
                   child: Container(
@@ -26,10 +49,29 @@ class HomePage extends StatelessWidget {
                       child: Text("Tap Me", style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.w500,
-                      ),),
+                      ),
+                      ),
                     ),
                   ),
-                )
+                ),
+                GestureDetector(
+                  onTap: (){},
+                  child: Container(
+                    margin: const EdgeInsets.all(10),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.green,
+                    ),
+                    child: const Center(
+                      child: Text("Tap Me", style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           )
